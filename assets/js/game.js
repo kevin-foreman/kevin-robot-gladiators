2a15902e7366eb293d6b945dde7c698cd864dcf6
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-02-15 20:55:54 -0600

    added loop elements

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,11 +19,13 @@ for(var i = 0; i < enemyNames.length; i++) {
 }
 
 // fight function
-var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+  // repeat and execute as long as the enemy-robot is alive
+  while(enemyHealth > 0) {
+    // place fight function code block here . . . 
+    var fight = function(enemyName) {
+  }
 
-  // ask player if they'd like to fight or run
+    // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
   // if player choses to fight, fight
