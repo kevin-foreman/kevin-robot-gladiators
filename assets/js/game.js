7d6e1a9d9eaadb8e389c4ecba9b061ca3702f877
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-02-15 20:10:27 -0600

    added for loops for multiple robot engagements

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,12 +7,19 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// Playing with for loops
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
+
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -57,6 +64,7 @@ var fight = function() {
       // subtract money from playerMoney for skipping
       playerMoney = playerMoney - 2;
     }
+
     // if no (false), ask question again by running fight() again
     else {
       fight();
@@ -65,7 +73,9 @@ var fight = function() {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
-};
+}
 
 // run fight function to start game
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
